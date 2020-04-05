4b3be09b77af4da96632811d9efcd3027070b7ef
HelenOne <h.bright@yandex.ru>
2020-04-05 19:41:29 +0300

    init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import launchInformation from "../launch-list.json";
+const Index = () => {
+  return (
+    <div>
+      <ul></ul>
+    </div>
+  );
+};
+ReactDOM.render(<Index />, document.querySelector("#root"));
